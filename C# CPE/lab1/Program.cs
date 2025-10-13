dc89dc5c5a5bba67168139b126042e81be8c78db
Andrei Astapchenia <andrey.ostapchenya@gmail.com>
2025-10-13 17:00:01 +0300

    Create file C#, added lab1 and new lab2

---
+++ C# CPE/lab1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+
+class Program
+{
+    static Dictionary<string, Color> Colors = new Dictionary<string, Color>(StringComparer.OrdinalIgnoreCase)
+    {
+        { "красн", Color.Red },
+        { "ал", Color.Crimson },
+        { "багр", Color.DarkRed },
+        { "зелен", Color.Green },
+        { "изумруд", Color.MediumSeaGreen },
+        { "малахит", Color.MediumSeaGreen },
+        { "син", Color.Blue },
+        { "голуб", Color.LightBlue },
+        { "лазур", Color.LightSkyBlue },
+        { "ультрамарин", Color.Blue },
+        { "желт", Color.Yellow },
+        { "золот", Color.Gold },
+        { "лимон", Color.LemonChiffon },
+        { "бел", Color.White },
+        { "черн", Color.Black },
+        { "сер", Color.Gray },
+        { "фиолетов", Color.Purple },
+        { "лилов", Color.Purple },
+        { "оранжев", Color.Orange },
+        { "коричнев", Color.Brown },
+        { "розов", Color.Pink },
+        { "бирюз", Color.Turquoise },
+    };
+    static Dictionary<string, string[]> ColorEndings = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase)
+    {
+        { "красн", new[] { "ый", "ая", "ое", "ые", "енький", "оватый", "" } },
+        { "ал", new[] { "ый", "ая", "ое", "ые", "" } },
+        { "багр", new[] { "овый", "ая", "ое", "ые", "" } },
+        { "зелен", new[] { "ый", "ая", "ое", "ые", "енький", "оватый", "" } },
+        { "изумруд", new[] { "ный", "ная", "ное", "ные", "" } },
+        { "малахит", new[] { "овый", "ая", "ое", "ые", "" } },
+        { "син", new[] { "ий", "яя", "ее", "ие", "енький", "" } },
+        { "голуб", new[] { "ой", "ая", "ое", "ые", "енький", "" } },
+        { "лазур", new[] { "ный", "ная", "ное", "ные", "" } },
+        { "ультрамарин", new[] { "овый", "ая", "ое", "ые", "" } },
+        { "желт", new[] { "ый", "ая", "ое", "ые", "енький", "оватый", "" } },
+        { "золот", new[] { "ой", "ая", "ое", "ые", "истый", "" } },
+        { "лимон", new[] { "ный", "ная", "ное", "ные", "" } },
+        { "бел", new[] { "ый", "ая", "ое", "ые", "енький", "" } },
+        { "черн", new[] { "ый", "ая", "ое", "ые", "енький", "" } },
+        { "сер", new[] { "ый", "ая", "ое", "ые", "енький", "" } },
+        { "фиолетов", new[] { "ый", "ая", "ое", "ые", "" } },
+        { "лилов", new[] { "ый", "ая", "ое", "ые", "" } },
+        { "оранжев", new[] { "ый", "ая", "ое", "ые", "" } },
+        { "коричнев", new[] { "ый", "ая", "ое", "ые", "" } },
+        { "розов", new[] { "ый", "ая", "ое", "ые", "енький", "" } },
+        { "бирюз", new[] { "овый", "ая", "ое", "ые", "" } },
+    };
+
+    static void Main()
+    {
+        string filePath = @"D:\csharp\1lab\ColorInTheText\";
+        string textName = "aeroport.txt";
+
+        string text = File.ReadAllText(filePath+textName);
+        Console.WriteLine("Файл загружен");
+        text = text.ToLower();
+        string[] splittedText = Regex.Split(text, @"[\p{P}\s\r\n]+");
+        Console.WriteLine($"Распознано {splittedText.Length} слов");
+
+        List<Color> foundColors = FindColorsInText(splittedText);
+        Console.WriteLine($"Найдено {foundColors.Count} цветов:");
+        var colorGroups = foundColors.GroupBy(c => c.Name)
+                                    .OrderByDescending(g => g.Count());
+
+        foreach (var group in colorGroups)
+        {
+            Console.WriteLine($"{group.Key}: {group.Count()} раз");
+        }
+        graphics.Clear(Color.White);
+        int x = 0, y = 0;
+        foreach (Color color in foundColors)
+        {
+            DrawPixel(color, ref x, ref y);
+        }
+        string imagePath = filePath + "Colors.png";
+        bitmap.Save(imagePath, ImageFormat.Png);
+        Console.WriteLine($"\nИзображение сохранено: {imagePath}");
+    }
+    static Bitmap bitmap = new Bitmap(100, 100);
+    static Graphics graphics = Graphics.FromImage(bitmap);
+    static SolidBrush brush = new SolidBrush(Color.White);
+
+
+    static List<Color> FindColorsInText(string[] words)
+    {
+        List<Color> foundColors = new List<Color>();
+        foreach (string word in words)
+        {
+            if (string.IsNullOrWhiteSpace(word) || word.Length < 3)
+                continue;
+
+            foreach (var colorBase in ColorEndings)
+            {
+                string baseWord = colorBase.Key;
+                string[] endings = colorBase.Value; 
+
+                if (word.StartsWith(baseWord))
+                {
+                    string restOfWord = word.Substring(baseWord.Length);
+                    foreach (string ending in endings)
+                    {
+                        if (restOfWord == ending)
+                        {
+                            if (Colors.TryGetValue(baseWord, out Color color))
+                            {
+                                foundColors.Add(color);
+                            }
+                            break;
+                        }
+                    }
+                    break; 
+                }
+            }
+        }
+        return foundColors;
+    }
+
+    static void DrawPixel(Color color, ref int x, ref int y, int imageSize = 100, int pixelSize = 10)
+    {
+        brush.Color = color;
+        graphics.FillRectangle(brush, x, y, pixelSize, pixelSize);
+
+        x += pixelSize;
+        if (x >= imageSize)
+        {
+            x = 0;
+            y += pixelSize;
+        }
+    }
+}
---
+++ C# CPE/lab2/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+struct GeneticData
+{
+    public string protein;
+    public string organism;
+    public string amino_acids;
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<GeneticData> proteins = new List<GeneticData>();
+        string[] lines = File.ReadAllLines("sequences.txt");
+        string[] commands = File.ReadAllLines("commands.txt");
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            string[] parts = line.Split('\t');
+            if (parts.Length == 3) {
+                GeneticData data = new GeneticData();
+                data.protein = parts[0];
+                data.organism = parts[1];
+                data.amino_acids = parts[2];
+                proteins.Add(data);
+            }
+        }
+        using (StreamWriter output = new StreamWriter("genedata.txt"))
+        {
+            output.WriteLine("Генетический поиск");
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string command = commands[i];
+                output.WriteLine(new string('-', 100));
+                string[] commandParts = command.Split('\t');
+                output.Write($"{(i + 1).ToString("D3")}");
+                for (int j = 0; j < commandParts.Length; j++) {
+                    output.Write($"\t{commandParts[j]}");
+                }
+                output.WriteLine();
+                if (commandParts.Length > 0)
+                {
+                    string operation = commandParts[0];
+                    switch (operation)
+                    {
+                        case "search":
+                            if (commandParts.Length > 1)
+                            {
+                                string searchSequence = commandParts[1];
+                                string decoded = searchSequence;
+                                if (HasDigits(searchSequence))
+                                {
+                                    decoded = RLD(searchSequence);
+                                }
+                                SearchOperation(proteins, decoded, output);
+                            }
+                            break;
+                        case "diff":
+                            if (commandParts.Length > 2)
+                            {
+                                DiffOperation(proteins, commandParts[1], commandParts[2], output);
+                            }
+                            break;
+                        case "mode":
+                            if (commandParts.Length > 1)
+                            {
+                                ModeOperation(proteins, commandParts[1], output);
+                            }
+                            break;
+                    }
+                }
+            }
+            output.WriteLine(new string('-', 100));
+        }
+        Console.WriteLine("Результат записан в genedata.txt");
+    }
+
+    static string RLD(string Sequence)
+    {
+        string result = ""; //string.Empty
+        int numberOFsymbol = 0;
+        while (numberOFsymbol < Sequence.Length)
+        {
+            char symbol = Sequence[numberOFsymbol];
+            if (symbol >= '1' && symbol <= '9') 
+            {
+             int countOFacids = int.Parse(symbol.ToString());
+                numberOFsymbol++;
+                char acid = Sequence[numberOFsymbol];
+                for (int i = 0; i < countOFacids; i++)
+                {
+                    result += acid;
+                }
+            }
+            else
+            {
+                result += symbol;
+            }
+            numberOFsymbol++;
+        }
+        return result;
+    }
+    static string RLE(string Sequence)
+        {
+            if (Sequence == "") return "";
+            string result = "";
+            int count = 1;
+            char currentAcid = Sequence[0];
+            for (int i = 1; i < Sequence.Length; i++)
+            {
+                if (Sequence[i] == currentAcid)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 2)
+                    {
+                        result += count.ToString() + currentAcid;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < count; j++)
+                        {
+                            result += currentAcid;
+                        }
+                    }
+                    count = 1;
+                    currentAcid = Sequence[i];
+                }
+            }
+            if (count > 2)
+            {
+                result += count.ToString() + currentAcid;
+            }
+            else
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    result += currentAcid;
+                }
+            }
+        return result;
+        }
+    static bool HasDigits(string Sequence)
+        {
+            foreach (char c in Sequence)
+            {
+                if (c >= '0' && c <= '9') return true;
+            }
+            return false;
+        }
+    static void SearchOperation(List<GeneticData> proteins, string searchSequence, StreamWriter output)
+    {
+        bool found = false;
+        foreach (GeneticData data in proteins)
+        {
+            if (data.amino_acids.Contains(searchSequence))
+            {
+                output.WriteLine($"organism\t protein");
+                output.WriteLine($"{data.organism}\t{data.protein}");
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            output.WriteLine("Не найдено");
+        }
+    }
+    static void DiffOperation(List<GeneticData> proteins, string protein1, string protein2, StreamWriter output)
+    {
+        GeneticData found1 = new GeneticData();
+        GeneticData found2 = new GeneticData();
+        bool foundProtein1 = false;
+        bool foundProtein2 = false;
+        foreach (GeneticData data in proteins)
+        {
+            if (data.protein == protein1)
+            {
+                found1 = data;
+                foundProtein1 = true;
+            }
+            if (data.protein == protein2)
+            {
+                found2 = data;
+                foundProtein2 = true;
+            }
+        }
+        if (!foundProtein1 || !foundProtein2)
+        {
+            output.Write("MISSING: ");
+            if (!foundProtein1) output.Write(protein1 + " ");
+            if (!foundProtein2) output.Write(protein2);
+            output.WriteLine();
+            return;
+        }
+        string sequence1 = found1.amino_acids;
+        string sequence2 = found2.amino_acids;
+        int minLength = Math.Min(sequence1.Length, sequence2.Length);
+        int difference = 0;
+
+        for (int i = 0; i < minLength; i++)
+        {
+            if (sequence1[i] != sequence2[i]) difference++;
+        }
+
+        difference += Math.Abs(sequence1.Length - sequence2.Length);
+        output.WriteLine($"amino-acids difference:\n {difference}");
+    }
+    static void ModeOperation(List<GeneticData> proteins, string proteinName, StreamWriter output)
+    {
+        GeneticData foundProtein = new GeneticData();
+        bool found = false;
+
+        foreach (GeneticData data in proteins)
+        {
+            if (data.protein == proteinName)
+            {
+                foundProtein = data;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            output.WriteLine($"MISSING: {proteinName}");
+            return;
+        }
+        string sequence = foundProtein.amino_acids;
+        int[] counts = new int[26]; // A - Z
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        foreach (char acid in sequence)
+        {
+            int index = alphabet.IndexOf(acid);
+            if (index >= 0)
+            {
+                counts[index]++;
+            }
+        }
+        int maxCount = 0;
+        for (int i = 0; i < counts.Length; i++)
+        {
+            if (counts[i] > maxCount)
+            {
+                maxCount = counts[i];
+            }
+        }
+        for (int i = 0; i < counts.Length; i++)
+        {
+            if (counts[i] == maxCount)
+            {
+                char mostCommonAcid = alphabet[i];
+                output.WriteLine($"amino-acid occurs:\n{mostCommonAcid} = {maxCount}");
+                return;
+            }
+        }
+
+
+    }
+}
